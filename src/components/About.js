10afea3aeb33bf62e27175009d52ed7c0c9172f5
Erickcamp <thecampkid@gmail.com>
2020-08-05 18:52:36 -0600

    forgot to save one component

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
         problems and building any idea out there. I love the feeling of being stumped and after hard
         work, finding the solution. Nothing is impossible! I love the idea of continuing to learn
         and the vast amount of possibilities there are within the development world. I am a proud
-        father of three, a lucky Romeo to my Juliet since high school, and a video game enthusiast!{" "}
+        father of three, a lucky Romeo to my Juliet since high school, and a video game enthusiast!
       </p>
     </div>
   )
